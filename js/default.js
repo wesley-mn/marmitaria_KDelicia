27f828ccc05b6f3a6b0b86e15c30c5a19efc8aa9
wesley-mn <wesleypiraquara2007ps4@gmail.com>
2026-03-20 19:35:00 -0300

    reformulando

--- js/default.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu');
-
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active'); // Liga/desliga a classe 'active'
-});
