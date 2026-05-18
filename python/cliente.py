74ce6340076947355ec3befe10a9b095d6c2a7d4
wesley-mn <wesleypiraquara2007ps4@gmail.com>
2026-05-18 19:58:05 -0300

    reforma do banco de dados

--- python/cliente.py
+++
@@ -1,9 +0,0 @@
-class Cliente:
-    def __init__(self, nome, telefone, endereco):
-        self.nome = nome
-        self.telefone = telefone
-        self.endereco = endereco
-
-    def __exibir_dados(self):
-        print(f"Nome: {self.nome}")
-        print(f"Telefone: {self.telefone}")
