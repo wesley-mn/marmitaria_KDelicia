b02c0a5c351cff5a1cb008af6681876f1098b098
wesley-mn <wesleypiraquara2007ps4@gmail.com>
2026-06-09 07:23:46 -0300

    update cardapio

--- js/compras.js
+++ js/compras.js
@@ -3,7 +3,7 @@
 const cardapioDiario = {
     0: [],
     1: ['File de Frango a  Milanesa', 'Bife na Chapa', 'Bisteca grelhada'],
-    2: ['Filé de Frango Grelhado a Cavalo', 'Bife na Chapa', 'Fígado Acebolado'],
+    2: ['Coxinha de Frango Crocante', 'Filé de Tilápia', 'Bisteca na Chapa'],
     3: ['Patinho Moído com Batata', 'Bisteca a Milanesa', 'Filé de Frango KFC'],
     4: ['Tirinhas de Bife com Pimentão e Cebola', 'Filé de Frango KFC', 'Filé de Frango a Parmegiana'],
     5: ['Feijoada', 'Strogonoff de Carne', 'Filé de Frango a Parmegiana'],
