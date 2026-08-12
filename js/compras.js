d0807d20835565bebb48048f8f3f75907e27887d
wesley-mn <wesleypiraquara2007ps4@gmail.com>
2026-08-11 22:35:43 -0300

    update

--- js/compras.js
+++ js/compras.js
@@ -4,7 +4,7 @@ const cardapioDiario = {
     0: [],
     1: ['Tirinhas de Alcatra Acebolada', 'Coxinha de Frango na Mostarda'],
     2: ['Bisteca Grelhada', 'Filé de Frango KFC', 'Filé de Frango a Parmegiana'],
-    3: ['Bisteca a Milanesa', 'Filé de Frango KFC', 'Fígado Acebolado'],
+    3: ['Bisteca a Milanesa', 'Coxa e Sobrecoxa ao Molho', 'Fígado Acebolado'],
     4: ['Coxinha de Frango', 'Pernil Acebolado', 'Carne Moída com Batata'],
     5: ['Feijoada', 'Bisteca a Milanesa', 'Bisteca a Parmegiana', 'Filé de Frango a Milanesa', 'Filé de Frango a Parmegiana'],
     6: ['Feijoada', 'Contra Filé Acebolado', 'Filé de Frango a Parmegiana', 'Filé de Frango a Milanesa'],
